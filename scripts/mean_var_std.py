0bc6b44dc1326d7c127b8b85e504a35dda45d1b0
ognjenstrbanovic <ogistrbanovic@gmail.com>
2020-10-12 17:39:19 -0400

    routine setup

--- scripts/mean_var_std.py
+++ scripts/mean_var_std.py
@@ -2,7 +2,10 @@ import numpy as np
 
 def calculate(list):
 
-
-
+    mean = np.mean(list)
+    variance = np.var(list)
+    standard_deviation = np.std(list)
+    maximum = np.max(list)
+    minimum = np.min(list)
 
     return calculations
